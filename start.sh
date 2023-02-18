078bbf1d2734528397b252bb5b3767b13869e8d8
LionKettyUD <116444594+LionKetty-UD@users.noreply.github.com>
2023-02-18 17:56:31 +0530

    like MEB

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/LionKetty-UD/Mother-earth-bot.git /Mother-earth-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Mother-earth-bot
 fi
-cd /PiroAutoFilterBot
+cd /Mother-earth-bot
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Mother-earth-bot...."
 python3 bot.py
